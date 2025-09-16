07b5082ba85067e5e3e140250f432e69de303615
Lev <kavardakfix228@gmail.com>
2025-09-16 17:06:56 +0300

    Реализованы 2 и 3 пункт меню

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <stdlib.h>
 #include <clocale>
 #include <string>
 using namespace std;
@@ -27,9 +28,22 @@ Pipe NewPipe(string name, float lenght, int diametr, bool repair) {
     return truba;
 };
 
+Ks NewKs(string name, int workshops_count, int workshops_working, string type) {
+    Ks station;
+    station.name = name;
+    station.workshops_count = workshops_count;
+    station.workshops_working = workshops_working;
+    station.type = type;
+
+    return station;
+};
+
 bool toFloat(string str) {
     try {
-        stof(str);
+        if (stof(str) < 0) {
+            cerr << "Ошибка: Число должно быть положительным!" << endl;
+            return false;
+        }
         return true;
     }
     catch (const invalid_argument& e) {
@@ -44,7 +58,10 @@ bool toFloat(string str) {
 
 bool toInt(string str) {
     try {
-        stoi(str);
+        if (stoi(str) < 0) {
+            cerr << "Ошибка: Число должно быть положительным" << endl;
+            return false;
+        }
         return true;
     }
     catch (const invalid_argument& e) {
@@ -64,6 +81,7 @@ bool toBool(string str) {
     cerr << "Ошибка: используйте только да или нет." << endl;
     return false;  
 }
+
 Pipe AddPipe() {
     string name;
     string lenght_nochecked;
@@ -92,7 +110,7 @@ Pipe AddPipe() {
         cin >> diametr_nochecked;
 
         if (toInt(diametr_nochecked)) {
-            diametr = stof(diametr_nochecked);
+            diametr = stoi(diametr_nochecked);
             break;
         }
     }
@@ -109,10 +127,81 @@ Pipe AddPipe() {
     }
 
     Pipe truba = NewPipe(name, lenght, diametr, repair);
+    system("cls");
     cout << "Труба успешно создана!" << endl << endl;
 
     return truba;
 
+}
+
+Ks AddKs() {
+    string name;
+    string workshops_count_nochecked;
+    string workshops_working_nochecked;
+    string type;
+
+    int workshops_count;
+    int workshops_working;
+
+    cout << "Название: ";
+    cin >> name;
+
+    while (true) {
+        cout << "Количество цехов: ";
+        cin >> workshops_count_nochecked;
+
+        if (toInt(workshops_count_nochecked)) {
+            workshops_count = stoi(workshops_count_nochecked);
+            break;
+        }
+    }
+
+    while (true) {
+        cout << "Количество цехов в работе: ";
+        cin >> workshops_working_nochecked;
+
+        if (toInt(workshops_working_nochecked)) {
+            workshops_working = stoi(workshops_working_nochecked);
+            if (workshops_working > workshops_count) {
+                cout << "Количество цехов в работе должно быть <= количества цехов." << endl;
+            }
+            else { break; }
+        }
+    }
+
+    cout << "Тип станции: ";
+    cin >> type;
+
+    Ks station = NewKs(name, workshops_count, workshops_working, type);
+    system("cls");
+    cout << "Станция успешно создана!" << endl << endl;
+
+    return station;
+
+}
+
+void ShowObjects(Pipe truba, Ks station) {
+    system("cls");
+    cout << "Объекты: ";
+
+    if (truba.name == "" && station.name == "") {
+        cout << "нет" << endl;
+        return;
+    }
+
+    if (truba.name != "") {
+        cout << endl << "Название: " << truba.name << endl;
+        cout << "Длина: " << truba.lenght << endl;
+        cout << "Диаметр: " << truba.diametr << endl;
+        cout << "В работе: " << truba.repair << endl << endl;
+    }
+
+    if (station.name != "") {
+        cout << endl << "Название: " << station.name << endl;
+        cout << "Количество цехов: " << station.workshops_count;
+        cout << " (" << station.workshops_working << " в работе)" << endl;
+        cout << "Тип: " << station.type << endl << endl;
+    }
 }
 
 void Menu() {
@@ -120,7 +209,7 @@ void Menu() {
     Ks station;
 
     while (true) {
-        cout << "Меню:\n1. Добавить трубу\n2. Добавить КС\n3. Показать объекты\n4. Редактировать трубу\n5. Редактировать КС\n6. Сохранить\n7. Загрузить\n0. Выход\n";
+        cout << "Меню:\n1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов\n4. Редактировать трубу\n5. Редактировать КС\n6. Сохранить\n7. Загрузить\n0. Выход\n";
         int choice;
         cin >> choice;
 
@@ -129,6 +218,13 @@ void Menu() {
             return;
         case 1:
             truba = AddPipe();
+            break;
+        case 2:
+            station = AddKs();
+            break;
+        case 3:
+            ShowObjects(truba, station);
+            break;
         };
     };
 };
